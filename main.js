4b888fea812816d20c5279e90e0bdcc720c12819
abdelrahmanashraf11coding <abdelrahmanashraf11coding>
2024-08-17 17:24:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var img = document.querySelector(".img");
+var container = document.querySelector(".container");
+function phone(phone){
+  img.src =phone;
+}
+function color(color){
+  container.style.backgroundColor = color
+}
